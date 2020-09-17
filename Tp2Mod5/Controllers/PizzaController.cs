29036435d4ddde1538fff02b51464c4160742370
Leslie BILECKOT <leslie.bileckot@gmail.com>
2020-09-17 17:02:30 +0200

    Correction tp2Mod5

--- Tp2Mod5/Controllers/PizzaController.cs
+++ Tp2Mod5/Controllers/PizzaController.cs
@@ -39,8 +39,6 @@ namespace Tp2Mod5.Controllers
             try
             {
                 // TODO: Add insert logic here
-                //vm.Chat.Couleur = FakeDbCat.Instance.Couleurs.FirstOrDefault(x => x.Id == vm.IdCouleur);
-                //FakeDbCat.Instance.Chats.Add(vm.Chat);
                 pVM.pizza.Pate = FakeDBPizza.Instance.ListePates.FirstOrDefault(x => x.Id == pVM.IdPate);
                 foreach (var item in pVM.IdIngredients)
                 {
@@ -48,7 +46,12 @@ namespace Tp2Mod5.Controllers
                     pVM.pizza.Ingredients.Add(ingredient);
                 }
                 Console.WriteLine(pVM.pizza);
+
+                //setter l'Id
+                pVM.pizza.Id = FakeDBPizza.Instance.ListePizza.Count == 0 ? 1 : FakeDBPizza.Instance.ListePizza.Max(x => x.Id) + 1;
+
                 FakeDBPizza.Instance.ListePizza.Add(pVM.pizza);
+
                 return RedirectToAction("Index");
             }
             catch
@@ -64,6 +67,17 @@ namespace Tp2Mod5.Controllers
         {
             PizzaViewModel pizzaVM = new PizzaViewModel();
             pizzaVM.pizza = FakeDBPizza.Instance.ListePizza.FirstOrDefault(x => x.Id == id);
+
+            if (pizzaVM.pizza.Pate != null)
+            {
+                pizzaVM.IdPate = pizzaVM.pizza.Pate.Id;
+            }
+            
+            if (pizzaVM.pizza.Ingredients.Any())
+            {
+                pizzaVM.IdIngredients = pizzaVM.pizza.Ingredients.Select(x => x.Id).ToList(); //select tt les id des ingredients               
+            }
+
             pizzaVM.ListePates = FakeDBPizza.Instance.ListePates;
             pizzaVM.ListeIngredients = FakeDBPizza.Instance.ListeIngredients;
             return View(pizzaVM);
@@ -71,12 +85,16 @@ namespace Tp2Mod5.Controllers
 
         // POST: Pizza/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(PizzaViewModel pVM)
         {
             try
             {
                 // TODO: Add update logic here
-
+                BO.Pizza pizza = FakeDBPizza.Instance.ListePizza.FirstOrDefault(x => x.Id == pVM.pizza.Id);
+                pizza.Nom = pVM.pizza.Nom;
+                pizza.Pate = FakeDBPizza.Instance.ListePates.FirstOrDefault(x => x.Id == pVM.IdPate);
+                pizza.Ingredients = FakeDBPizza.Instance.ListeIngredients.Where(x => pVM.IdIngredients.Contains(x.Id)).ToList();
+                
                 return RedirectToAction("Index");
             }
             catch
@@ -88,7 +106,9 @@ namespace Tp2Mod5.Controllers
         // GET: Pizza/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            PizzaViewModel pizzaVM = new PizzaViewModel();
+            pizzaVM.pizza = FakeDBPizza.Instance.ListePizza.FirstOrDefault(x => x.Id == id);
+            return View(pizzaVM);
         }
 
         // POST: Pizza/Delete/5
